c2a564caa6a8b87c74d4b1703359e2fb14631fb7
Andre Torres <andre855@gmail.com>
2025-03-09 11:43:10 +0100

    Add Delete score button

--- src/SheetMaker/ListScores.tsx
+++ src/SheetMaker/ListScores.tsx
@@ -1,8 +1,10 @@
 import { Button, Dialog, Flex, Heading, Separator, Text, TextField } from "@radix-ui/themes";
 import { useLiveQuery } from "dexie-react-hooks";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import React from "react";
 import { useURLHash } from "../hooks/useURLHash";
 import { db } from "../lib/storage";
+import type { SavedScore, Score } from "../lib/types";
 
 export function ListScores() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,6 +12,17 @@ export function ListScores() {
   const scores = useLiveQuery(async () => {
     return await db.scores.toArray();
   });
+
+  const loadScore = useCallback((score: Score) => {
+    const newHash = new URLSearchParams(hash);
+    newHash.set("score", JSON.stringify(score));
+    setHash(newHash);
+    setIsOpen(false);
+  }, [hash, setHash]);
+
+  const deleteScore = useCallback(async (id: number) => {
+    await db.scores.delete(id);
+  }, []);
 
   return <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
     <Dialog.Trigger>
@@ -21,28 +34,46 @@ export function ListScores() {
 
       <Flex direction="column" gap="2">
         {scores?.map((score, index) => {
-          // biome-ignore lint/correctness/useJsxKeyInIterable: <explanation>
-          return <><Flex justify="between">
-            <Flex direction="column">
-              <Text as="div" weight="bold">{score.name}</Text>
-              <Flex gap="3">
-                <Text>{score.beats}BPM</Text>
-                -
-                <Text>1/{score.notes}</Text>
-              </Flex>
-            </Flex>
-
-            <Button onClick={() => {
-              const newHash = new URLSearchParams(hash);
-              newHash.set("score", JSON.stringify(score.score));
-              setHash(newHash);
-              setIsOpen(false);
-            }} style={{ alignSelf: "center"}}>Load</Button>
-          </Flex>
-          {(index < scores.length - 1) && <Separator size="4" orientation="horizontal" />}
-          </>
+          return (<React.Fragment key={score.id}> 
+            <ScoreRow  score={score} onLoad={() => loadScore(score.score) } onDelete={() => { deleteScore(score.id) }} />
+            {index < scores.length - 1 && <Separator size="4" orientation="horizontal" />}
+          </React.Fragment>)
         })}
       </Flex>
     </Dialog.Content>
   </Dialog.Root>
+}
+
+interface ScoreRowProps {
+  score: SavedScore & { id: number },
+  onLoad?: () => void;
+  onDelete?: () => void;
+}
+
+function ScoreRow({ score, onLoad, onDelete }: ScoreRowProps) {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  if (isDeleting) {
+    return <Flex justify="between" minHeight="50px" align="center">
+        <Text as="div">Do you really want to delete this score?</Text>
+        <Flex align="center" gap="3">
+          <Button onClick={() => { onDelete?.() }}>Yes</Button>
+          <Button onClick={() => { setIsDeleting(false) }} variant="soft">No</Button>
+        </Flex>
+      </Flex>
+  }
+  return <Flex justify="between" minHeight="50px">
+      <Flex direction="column">
+        <Text as="div" weight="bold">{score.name}</Text>
+        <Flex gap="3">
+          <Text>{score.beats}BPM</Text> - <Text>1/{score.notes}</Text>
+        </Flex>
+      </Flex>
+
+      <Flex align="center" gap="3">
+        <Button onClick={() => { onLoad?.() }}>Load</Button>
+        {onDelete && <Button onClick={() => { setIsDeleting(true) }} color="crimson" variant="soft">Delete</Button>}
+      </Flex>
+    </Flex>
+
 }
--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -3,7 +3,7 @@ import type { SavedScore } from "./types";
 
 
 export const db = new Dexie("Metronome") as Dexie & {
-    scores: EntityTable<SavedScore>
+    scores: EntityTable<SavedScore & { id: number }, "id">
 };
 
 db.version(1).stores({
