d63fc21172ac8821fcded32e91dc80ebce4beb34
Andre <andre.guelfi.torres@proton.me>
2026-08-01 13:39:59 +0200

    Fix use-score-interval hook to handle different types of tempos

--- src/pages/metronome/ui/use-audio-tick.ts
+++ src/pages/metronome/ui/use-audio-tick.ts
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { Sampler, ToneAudioBuffer, now } from "tone";
 import { calculateBeatTime } from "../model/beat-time";
 import { nextInLoop } from "~/shared/lib/loop";
+import { useScoreStore } from "~/entities/score/model/state/score-store-provider";
 
 interface UseAudioTicksProps {
   notes: number;
--- src/pages/metronome/ui/use-score-interval.ts
+++ src/pages/metronome/ui/use-score-interval.ts
@@ -4,38 +4,64 @@ import { useScoreStoreShallow } from "~/entities/score/model/state/score-store-p
 import { calculateBeatTime } from "../model/beat-time";
 
 export interface UseScoreIntervalProps {
-  onTick?: () => Promise<void> | void;
+  onTick?: () => Promise<void>;
 }
 
 export function useScoreInterval({ onTick }: UseScoreIntervalProps) {
-  const { tempo, bpm } = useScoreStoreShallow((state) => ({
-    tempo: state.configuration.signature,
+  const { bpm, score } = useScoreStoreShallow((state) => ({
     bpm: state.configuration.bpm,
+    score: state.score,
   }));
-
+  const flatScore = score.bars.flatMap((n) =>
+    n.parts.flatMap((part) => part.notes.map((note) => ({ note, tempo: part.tempo }))),
+  );
   const index = useRef(0);
-  const beatTime = calculateBeatTime(bpm, tempo);
   const [isToggled, toggle] = useToggle();
   const timeout = useRef<number | undefined>(undefined);
+  const scoreIndex = useRef(0);
 
-  const ticker = useCallback(() => {
-    onTick?.();
+  const ticker = useCallback(async () => {
+    await onTick?.();
+    const note = flatScore[scoreIndex.current];
+    if (!note) {
+      throw new Error("Missing note");
+    }
+    let beatTime: number;
+    switch (note.tempo) {
+      case "sixteens":
+        beatTime = calculateBeatTime(16, bpm);
+        break;
+      case "quarter":
+        beatTime = calculateBeatTime(4, bpm);
+        break;
+      case "eights":
+        beatTime = calculateBeatTime(8, bpm);
+        break;
+      default:
+        throw new Error("NOT SUPPORTED");
+    }
     index.current++;
+    scoreIndex.current++;
+    if (scoreIndex.current >= flatScore.length) {
+      scoreIndex.current = 0;
+    }
+
     timeout.current = setTimeout(() => {
-      ticker();
+      void ticker();
     }, beatTime);
-  }, [onTick]);
+  }, [onTick, flatScore, bpm]);
 
   useEffect(() => {
     if (!isToggled) {
       index.current = 0;
+      scoreIndex.current = 0;
       if (timeout.current) {
         clearTimeout(timeout.current);
       }
     } else {
-      ticker();
+      void ticker();
     }
-  }, [isToggled]);
+  }, [isToggled, ticker]);
 
   return { isToggled, toggle };
 }
