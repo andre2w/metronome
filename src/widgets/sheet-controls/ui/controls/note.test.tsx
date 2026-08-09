bc4990a7798aa1a25f6b023fecc000a96cd39fe0
Andre <andre.guelfi.torres@proton.me>
2026-08-09 10:07:34 +0200

    Add unit tests for Note component

---
+++ src/widgets/sheet-controls/ui/controls/note.test.tsx
@@ -0,0 +1,101 @@
+import { describe, expect, test } from "vitest";
+import { render } from "~/shared/test/render";
+import { Note } from "./note";
+import { page } from "vitest/browser";
+
+describe("Note", () => {
+  test("renders all keys for the note with count on top", async () => {
+    const component = await render(
+      <Note
+        index={{ barIndex: 0, noteIndex: 0, partIndex: 0 }}
+        notesWithSticking={{ type: "note", keys: [] }}
+        noteCount="1"
+      />,
+    );
+
+    const tileLocator = component.locator.getByRole("button").all();
+    expect(tileLocator).toHaveLength(10);
+    const stickingsTile = tileLocator.at(0)?.element();
+    await expect.element(stickingsTile!).toHaveTextContent("1");
+  });
+
+  test("allows to loop between stickings when clicking on the count", async () => {
+    const component = await render(
+      <Note
+        index={{ barIndex: 0, noteIndex: 0, partIndex: 0 }}
+        notesWithSticking={{ type: "note", keys: [] }}
+        noteCount="1"
+      />,
+    );
+
+    let stickingTile = component.locator.getByRole("button").first();
+    await expect.element(stickingTile).toHaveTextContent("1");
+    await stickingTile.click();
+
+    stickingTile = component.locator.getByRole("button").first();
+    expect(component.store.getState().score.bars.at(0)?.parts.at(0)?.notes.at(0)?.sticking).toEqual(
+      "L",
+    );
+    await expect.element(stickingTile).toHaveTextContent("L");
+
+    stickingTile = component.locator.getByRole("button").first();
+    await stickingTile.click();
+    expect(component.store.getState().score.bars.at(0)?.parts.at(0)?.notes.at(0)?.sticking).toEqual(
+      "R",
+    );
+    await expect.element(stickingTile).toHaveTextContent("R");
+
+    stickingTile = component.locator.getByRole("button").first();
+    await stickingTile.click();
+    expect(component.store.getState().score.bars.at(0)?.parts.at(0)?.notes.at(0)?.sticking).toEqual(
+      "R/L",
+    );
+    await expect.element(stickingTile).toHaveTextContent("R/L");
+
+    stickingTile = component.locator.getByRole("button").first();
+    await stickingTile.click();
+    expect(
+      component.store.getState().score.bars.at(0)?.parts.at(0)?.notes.at(0)?.sticking,
+    ).toBeUndefined();
+    await expect.element(stickingTile).toHaveTextContent("1");
+  });
+
+  test("add part to score when clicking on it", async () => {
+    const component = await render(
+      <Note
+        index={{ barIndex: 0, noteIndex: 0, partIndex: 0 }}
+        notesWithSticking={{ type: "note", keys: [] }}
+        noteCount="1"
+      />,
+    );
+
+    const snareButton = component.locator.getByRole("button", { name: "SNARE" });
+    await snareButton.click();
+
+    expect(component.store.getState().score.bars.at(0)?.parts.at(0)?.notes.at(0)).toEqual({
+      type: "note",
+      keys: [{ type: "key", note: "SNARE" }],
+    });
+  });
+
+  test("display modifiers when right clicking the note", async () => {
+    const component = await render(
+      <Note
+        index={{ barIndex: 0, noteIndex: 0, partIndex: 0 }}
+        notesWithSticking={{ type: "note", keys: [] }}
+        noteCount="1"
+      />,
+    );
+
+    const snareButton = component.locator.getByRole("button", { name: "SNARE" });
+    await snareButton.click({ button: "right" });
+
+    const ghostedSnare = page.getByText("Ghosted");
+    await expect.element(ghostedSnare).toBeVisible();
+    await ghostedSnare.click();
+    expect(component.store.getState().score.bars.at(0)?.parts.at(0)?.notes.at(0)).toEqual({
+      type: "note",
+      keys: [{ type: "key", note: "SNARE", modifier: "GHOST_SNARE" }],
+    });
+  });
+});
--- src/widgets/sheet-controls/ui/controls/tile.tsx
+++ src/widgets/sheet-controls/ui/controls/tile.tsx
@@ -1,22 +1,19 @@
-import { Box } from "@radix-ui/themes";
-import { ComponentRef, forwardRef, Key, ReactNode } from "react";
+import { Box, BoxProps } from "@radix-ui/themes";
+import { ComponentRef, forwardRef, ReactNode } from "react";
 import "./tile.css";
 
-export interface TileProps {
-  key?: Key;
-  className?: string;
-  onClick?: () => void | Promise<void>;
+export interface TileProps extends Exclude<BoxProps, { as: "span" }> {
   children: ReactNode;
   variant?: "selected";
 }
 
 export const Tile = forwardRef<ComponentRef<"div">, TileProps>(
-  ({ children, className, key, onClick, variant, ...props }, ref) => {
+  ({ children, className, onClick, variant, ...props }, ref) => {
     return (
       <Box
         {...props}
+        role={"button"}
         ref={ref}
-        key={key}
         height="35px"
         width="35px"
         className={`tile ${variant === "selected" ? "selected" : "not-selected"} ${className ?? ""}`}
